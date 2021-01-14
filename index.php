d5f049e30145c78205163314f46f3b6c2f33817c
Riie <rayderick27@gmail.com>
2021-01-14 15:14:05 +0700

    menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Coba GIT Remote</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic exercitationem delectus soluta doloremque vel! Itaque, voluptatum. Tempore id natus earum quasi dolore eius, quod fuga. Mollitia inventore incidunt ducimus harum?</p>
+</body>
+</html>
